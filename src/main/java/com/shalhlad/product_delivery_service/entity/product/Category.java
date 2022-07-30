056f83aa2cc95b3932757c87bd2453bef454be83
Kiryl <shakhlad26@gmail.com>
2022-07-31 01:00:51 +0300

    Category now has hibernate equals and hashcode methods

--- src/main/java/com/shalhlad/product_delivery_service/entity/product/Category.java
+++ src/main/java/com/shalhlad/product_delivery_service/entity/product/Category.java
@@ -1,5 +1,6 @@
 package com.shalhlad.product_delivery_service.entity.product;
 
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +10,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.Hibernate;
 
 @Getter
 @Setter
@@ -24,4 +26,21 @@ public class Category {
   @Column(unique = true, nullable = false, length = 50)
   private String name;
 
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+      return false;
+    }
+    Category category = (Category) o;
+    return id != null && Objects.equals(id, category.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
 }
