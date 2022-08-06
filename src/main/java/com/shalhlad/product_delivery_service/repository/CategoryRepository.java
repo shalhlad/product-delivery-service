1faea20ec95730988e2332bdfc7d4437bd326c6f
Kiryl <shakhlad26@gmail.com>
2022-08-07 00:43:45 +0300

    PagingAndSortingRepository interfaces with unused pagination methods have been replaced with JpaRepository

--- src/main/java/com/shalhlad/product_delivery_service/repository/CategoryRepository.java
+++ src/main/java/com/shalhlad/product_delivery_service/repository/CategoryRepository.java
@@ -1,10 +1,10 @@
 package com.shalhlad.product_delivery_service.repository;
 
 import com.shalhlad.product_delivery_service.entity.product.Category;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends PagingAndSortingRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
--- src/main/java/com/shalhlad/product_delivery_service/repository/DepartmentRepository.java
+++ src/main/java/com/shalhlad/product_delivery_service/repository/DepartmentRepository.java
@@ -1,10 +1,10 @@
 package com.shalhlad.product_delivery_service.repository;
 
 import com.shalhlad.product_delivery_service.entity.department.Department;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DepartmentRepository extends PagingAndSortingRepository<Department, Long> {
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
 }
