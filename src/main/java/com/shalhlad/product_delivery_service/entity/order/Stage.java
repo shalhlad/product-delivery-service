25875aaeab9aac0d56a9536ffdb316a5702358b6
Kiryl <shakhlad26@gmail.com>
2022-08-09 02:20:01 +0300

    added helper methods to Stage class

--- src/main/java/com/shalhlad/product_delivery_service/entity/order/Stage.java
+++ src/main/java/com/shalhlad/product_delivery_service/entity/order/Stage.java
@@ -1,11 +1,67 @@
 package com.shalhlad.product_delivery_service.entity.order;
 
 public enum Stage {
-  NEW,
-  APPROVED,
-  COLLECTING,
-  COLLECTED,
-  IN_DELIVERY,
-  GIVEN,
-  CANCELED
+
+  NEW {
+    @Override
+    public Stage next() {
+      return APPROVED;
+    }
+  },
+
+  APPROVED {
+    @Override
+    public Stage next() {
+      return COLLECTING;
+    }
+  },
+
+  COLLECTING {
+    @Override
+    public Stage next() {
+      return COLLECTED;
+    }
+  },
+
+  COLLECTED {
+    @Override
+    public Stage next() {
+      return IN_DELIVERY;
+    }
+  },
+
+  IN_DELIVERY {
+    @Override
+    public Stage next() {
+      return GIVEN;
+    }
+  },
+
+  GIVEN {
+    @Override
+    public Stage next() {
+      throw new UnsupportedOperationException("GIVEN is final stage");
+    }
+  },
+
+  CANCELED {
+    @Override
+    public Stage next() {
+      throw new UnsupportedOperationException("there is no next stage after CANCELED");
+    }
+  };
+
+  public abstract Stage next();
+
+  public boolean canBeChangedByCollector() {
+    return this == NEW ||
+        this == APPROVED ||
+        this == COLLECTING;
+  }
+
+  public boolean canBeChangedByCourier() {
+    return this == COLLECTED ||
+        this == IN_DELIVERY;
+  }
+
 }
